9aa047460170a022111e914cac2226e9ef5521bc
Alraune Zech <a.zech@uu.nl>
2025-03-15 23:02:12 +0100

    implement analysis and plotting of dispersivity statitics from Paper Zech et al 2022

---
+++ scripts/analysis/statistics.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 14 16:25:36 2025
+
+@author: alraune
+"""
+
+import numpy as np
+from scipy.special import erf
+
+def macrodispersivity_statistics(data_het_classes,
+                                 verbose = True
+                                 ):
+
+    for het_level in data_het_classes:
+        
+        aL_het = data_het_classes[het_level]['aL_het']
+        weights_het = data_het_classes[het_level]['weights_het']
+        
+        meanw_het = np.average(aL_het,weights=weights_het)
+        stdw_het = np.sqrt(np.average(aL_het**2,weights=weights_het)-meanw_het**2)
+        
+        mean_het = np.mean(aL_het)
+        std_het = np.std(aL_het)
+        
+        log_meanw = np.log(meanw_het**2/np.sqrt(meanw_het**2+stdw_het**2))
+        log_varw = np.log(1+(stdw_het/meanw_het)**2)
+
+        if verbose:
+            print("\nLevel of Heterogeneity: ", het_level)
+            print(" Number of sites: {}".format(len(aL_het)))    
+            print(" Mean of AL (weighted): {:.2f}".format(meanw_het))
+            print(" Std of AL: (weighted) {:.2f}".format(stdw_het))
+            # print(" Mean of AL (not weighted): {:.2f}".format(mean_het))
+            # print(" Std of AL (not weighted): {:.2f}".format(std_het))
+            print(" Coefficient of Variation CV: {:.2f}".format(stdw_het/meanw_het))
+
+        data_het_classes[het_level]['number_sites'] = len(aL_het)
+        data_het_classes[het_level]['mean_w_al'] = meanw_het
+        data_het_classes[het_level]['std_w_al'] = stdw_het
+        data_het_classes[het_level]['cv_w_al'] = stdw_het/meanw_het
+        data_het_classes[het_level]['mean_al'] = mean_het
+        data_het_classes[het_level]['std_al'] = std_het
+        data_het_classes[het_level]['log_meanw'] = log_meanw
+        data_het_classes[het_level]['log_varw'] = log_varw
+
+    return data_het_classes
+
+def cdf(x,log_mean,log_var):
+    """
+    Cumulative density function for log-normal distribution.
+
+    Input
+    -----
+    x : float or np.array of floats
+        argument values
+    log_mean: 
+        log-mean value
+    log_var: 
+        log-varience value
+
+    Output
+    ------
+    cdf: float or np.array of floats
+        function values for arguments    
+    """
+    
+    cdf = 0.5*(1+erf((np.log(x)-log_mean)/np.sqrt(2*log_var)))
+    return cdf
---
+++ scripts/visualize/plot_dispersivity_stats.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 14 16:30:22 2025
+
+@author: alraune
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('/home/alraune/GitHub/AlrauneZ/Macrodispersivity/')
+
+from scripts.analysis.statistics import macrodispersivity_statistics
+from scripts.analysis.statistics import cdf                    
+
+def plot_alphaL_cfd(data_het_classes,
+                    figsize=[5.4,3.2],
+                    marker = 'o',
+                    msize = 7, 
+                    mec = 'k',
+                    colors = ['C1','C2','C4'],
+                    lw=4,
+                    xlim = [0,13],
+                    ylim = [0,1],
+                    xlabel = r'Longitudinal dispersivity $\alpha_L$ [m]',
+                    ylabel = 'cumulative density',
+                    grid = True,
+                    textsize = 12,
+                    save_fig = None,
+                    ):
+    
+                    # label_R1 = 'high reliablity',
+                    # label_R2 = 'moderate reliablity',
+    
+    ### Plotting
+    fig = plt.figure(figsize=figsize)
+
+    macrodispersivity_statistics(data_het_classes,verbose = False)
+     
+    for index, (key, value) in enumerate(data_het_classes.items()):
+    
+        aL_het = value['aL_het']
+        plt.plot(np.sort(aL_het),
+                 np.linspace(0,1,len(aL_het)+2)[1:-1],
+                 marker = marker,
+                 mec = mec,
+                 ms=msize,
+                 ls='',
+                 c=colors[index],
+                 zorder = 3,
+                 label='data, level {}  '.format(key)
+                 )
+
+
+    alpha_range = np.arange(0.001,13,0.1)
+    for index, (key, value) in enumerate(data_het_classes.items()):
+        lognorm_cdf = cdf(alpha_range,value['log_meanw'],value['log_varw'])
+        plt.plot(alpha_range,
+                 lognorm_cdf,
+                 c=colors[index],
+                 lw=lw,
+                 zorder = 2,
+                 label='CDF'
+                 )
+    
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+    plt.legend(loc='lower right',
+               fontsize=textsize,
+               ncol=2,
+               facecolor='oldlace',
+               shadow = True)
+    plt.xlabel(xlabel,fontsize=textsize)    
+    plt.ylabel(ylabel,fontsize=textsize)    
+    plt.tick_params(axis="both",which="major",labelsize=textsize)
+    plt.grid(grid)
+    plt.tight_layout()
+
+    if isinstance(save_fig, str):
+        plt.savefig(save_fig,dpi=300)
+        print('Save figure to {}'.format(save_fig))
+    
+    return fig
